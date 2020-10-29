c9c408a82c40b8701e5083b4312a49616c27c17c
Antoine Maes <antoinemaes@gmail.com>
2020-10-29 20:55:06 +0100

    4) Serve static assets

--- myApp.js
+++ myApp.js
@@ -14,16 +14,20 @@ console.log("Hello World");
 
 
 /** 2) A first working Express Server */
-
+/*
 app.get('/', function(req, res) {
     res.send('Response String');
 })
-
+*/
 /** 3) Serve an HTML file */
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
 /** 4) Serve static assets  */
 
+app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 
