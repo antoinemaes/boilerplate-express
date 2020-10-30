812ef7651f34214193b19dd22ce2afdac49c3842
Antoine Maes <antoinemaes@gmail.com>
2020-10-30 07:17:55 +0100

    6) use .env

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -31,13 +31,22 @@ app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 
+/*
 app.get('/json', function (req, res) {
     res.json({message: "Hello json"});
 });
+*/
 
 /** 6) Use the .env file to configure the app */
  
- 
+app.get('/json', function (req, res) {
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({message: "HELLO JSON"});
+    } else {
+        res.json({message: "Hello json"});
+    }
+}); 
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
